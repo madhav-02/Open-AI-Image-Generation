56ad98a1772fd16f378f90695bc7782911955d0a
Madhav007 <madhavaprashath669@gmail.com>
2023-02-06 20:30:47 +0530

    Added components layout

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useEffect } from 'react';
+import {Card, Loader, FormField} from "../components"
+const Home = () => {
+  return (
+    
+    <div>Home</div>
+  )
+}
+
+export default Home
